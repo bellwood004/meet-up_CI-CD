1000f5259f81f032aca9ab3b7c141e377df380c4
bellwood004 <54351123+bellwood004@users.noreply.github.com>
2021-06-16 21:01:22 +0900

    Update home.blade.php

--- my-laravel-app/resources/views/home.blade.php
+++ my-laravel-app/resources/views/home.blade.php
@@ -4,8 +4,8 @@
 <div class="container">
     <div class="home-content">
         <ul>
-            <li><a href="{{ route('register_book') }}">本を登録</a></li>
-            <li><a href="{{ route('show_books') }}">登録一覧</a></li>
+            <li><a href="{{ route('register_book') }}">本</a></li>
+            <li><a href="{{ route('show_books') }}">覧</a></li>
         </ul>
     </div>
 </div>
